8e1b2131cf106a42b6be8dfb844edff20ebb4636
MungSirfalas <cesar.ron.1991@gmail.com>
2023-04-11 02:46:34 -0500

    Changes video capture system

--- video_capture/video_capture_system.py
+++ video_capture/video_capture_system.py
@@ -1,4 +1,21 @@
+import numpy as np
 import cv2
 
 #face_haar_cascade = cv2.CascadeClassifier(cv2.)
-cap = cv2.VideoCapture()
+cap = cv2.VideoCapture(0)
+
+#if not cap.isOpened():
+#    cap = cv2.VideoCapture(0)
+#if not cap.isOpened():
+#    raise IOError("Cannot open webcam")
+
+while True:
+    _, frame = cap.read()
+
+    cv2.imshow('detector', frame)
+
+    if cv2.waitKey(1) & 0xFF==ord("q"):
+        break
+
+cap.release()
+cv2.destroyAllWindows
